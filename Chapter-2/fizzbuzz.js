78b7fd6e788fe571e1e07ac229d90b62cb32d06b
ngpham0602 <pkbnguyen@gmail.com>
2025-07-10 21:03:07 +1000

    FizzBuzz

---
+++ Chapter-2/fizzbuzz.js
@@ -0,0 +1,13 @@
+result = 0
+for (count = 0; count < 100; count = count + 1) {
+    result = result + 1;
+    if (result % 3 == 0 && result % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (result % 5 == 0) {
+        console.log("Buzz");
+    } else if (result % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(result);        
+    }
+}
